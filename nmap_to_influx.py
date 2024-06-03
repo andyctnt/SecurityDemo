a21ae36e68ae95957fcc6f79c6f7e8fe18699915
Rubén Bautista <66707851+andyctnt@users.noreply.github.com>
2024-06-03 13:16:06 -0500

    Update nmap_to_influx.py

--- nmap_to_influx.py
+++ nmap_to_influx.py
@@ -21,27 +21,54 @@ def read_target_ips(file_path='/listado.list'):
 
 # Función para ejecutar Nmap y procesar los resultados
 def run_nmap(target_ips):
-    for ip in target_ips:
-        result = subprocess.run(['nmap', '-oX', '-', ip], stdout=subprocess.PIPE)
-        xml_output = result.stdout.decode()
-        root = ET.fromstring(xml_output)
-        json_body = []
+    ip_range = ' '.join(target_ips)
+    nmap_command = ['nmap', '-sV', '-F', '--script=http-title,ssl-cert', '-oX', 'myoutput.xml', ip_range]
+    result = subprocess.run(nmap_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+    # Leer el archivo myoutput.xml
+    with open('myoutput.xml', 'r') as xml_file:
+        xml_output = xml_file.read()
+    
+    root = ET.fromstring(xml_output)
+    json_body = []
 
-        for host in root.findall('host'):
-            address = host.find('address').get('addr')
-            status = host.find('status').get('state')
+    for host in root.findall('host'):
+        address = host.find('address').get('addr')
+        status = host.find('status').get('state')
+        for port in host.findall('ports/port'):
+            port_id = port.get('portid')
+            protocol = port.get('protocol')
+            service = port.find('service')
+            service_name = service.get('name') if service is not None else 'unknown'
+            service_version = service.get('version') if service is not None else 'unknown'
+            http_title = ''
+            ssl_cert = ''
+
+            # Extraer información del script de Nmap
+            for script in port.findall('script'):
+                if script.get('id') == 'http-title':
+                    http_title = script.get('output')
+                elif script.get('id') == 'ssl-cert':
+                    ssl_cert = script.get('output')
+            
             json_body.append({
                 "measurement": "nmap_scan",
                 "tags": {
-                    "host": address
+                    "host": address,
+                    "port": port_id,
+                    "protocol": protocol,
+                    "service": service_name
                 },
                 "fields": {
-                    "status": status
+                    "status": status,
+                    "service_version": service_version,
+                    "http_title": http_title,
+                    "ssl_cert": ssl_cert
                 }
             })
 
-        if json_body:
-            client.write_points(json_body)
+    if json_body:
+        client.write_points(json_body)
 
 # Ejecutar Nmap periódicamente
 def main():
