14f504678cd05d04246dd8efa54bf402fa9b49e8
Rubén Bautista <66707851+andyctnt@users.noreply.github.com>
2024-06-03 20:43:06 -0500

    Create wait-for-it.sh

---
+++ wait-for-it.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+host="$1"
+shift
+cmd="$@"
+
+until mysql -h "$host" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e 'select 1' &> /dev/null; do
+  >&2 echo "MySQL is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "MySQL is up - executing command"
+exec $cmd
